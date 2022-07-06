9d6af5332e085ee6e81d8418119bfca82f1fc5ed
VitorEmerique <vitort.emerique@hotmail.com>
2022-07-06 19:56:49 -0300

    Usando typecscript

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
+"use strict";
 let button = document.getElementById('button');
 let input1 = document.getElementById('input1');
 let input2 = document.getElementById('input2');
-
-function somarNumeros(numero1,numero2){
-   if(typeof numero1 ==='number' && typeof numero2 ==='number'){
-    return numero1+numero2;
-   }else{
-    return Number(numero1)+ Number(numero2);
-   }
-    
+function adicionarNumeros(n1, n2) {
+    return n1 + n2;
 }
-
-button.addEventListener('click',()=>{
-    console.log(somarNumeros(input1.value,input2.value));
-});
+if (button) {
+    button.addEventListener('click', () => {
+        if (input1 && input2) {
+            console.log(adicionarNumeros(Number(input1.value), Number(input2.value)));
+        }
+    });
+}

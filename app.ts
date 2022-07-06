e1d989644bf27586062ab9afc816adc85572b4c1
VitorEmerique <vitort.emerique@hotmail.com>
2022-07-06 20:52:50 -0300

    'tipo_de_variaveis'

--- app.ts
+++ app.ts
@@ -3,13 +3,22 @@ let input1 = document.getElementById('input1') as HTMLInputElement;
 let input2 = document.getElementById('input2')as HTMLInputElement;
 
 
-function adicionarNumeros(n1:number,n2:number){
+function adicionarNumeros(n1:number,n2:number,devePrintar:boolean,frase:string){
+    let resultado = n1+n2;
+    if(devePrintar){
+        console.log(frase + resultado)
+    }
     return n1 + n2;
 }
+
+let devePrintar = true;
+
+let frase = 'O valor é: ';
+
 if(button){
    button.addEventListener('click',()=>{
         if(input1 && input2){
-            console.log(adicionarNumeros(Number(input1.value),Number(input2.value)));
+            console.log(adicionarNumeros(Number(input1.value),Number(input2.value),devePrintar,frase));
         }
    }) 
 }

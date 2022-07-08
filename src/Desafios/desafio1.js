342bdbd820b816b19d607986eca9c5c024d90a4e
VitorEmerique <vitort.emerique@hotmail.com>
2022-07-07 23:36:48 -0300

    'Desafio3'

--- src/Desafios/desafio1.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Como podemos rodar isso em um arquivo .ts sem causar erros? 
-;
-let employee = {
-    code: 10,
-    name: "John"
-};
-console.log(employee.code);
-console.log(employee.name);
